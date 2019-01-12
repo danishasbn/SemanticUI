d23f2c1edb26c8701ed3abd48ad61c104d0ad3fb
danishasbn <soobhendanisha@gmail.com>
2019-01-12 17:45:12 +0400

    Semantic UI first commit

---
+++ common/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Semantic UI</title>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+  </head>
+  <body>
